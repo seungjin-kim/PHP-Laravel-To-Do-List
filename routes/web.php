4e818a21c8418af429e0107b11e014243920873c
vagrant <vagrant@homestead>
2020-06-16 21:16:47 +0000

    Stubbed the routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+Route::post('/task', function () {
+    //
+});
+
+Route::delete('/task/{id}', function () {
+    //
 });
