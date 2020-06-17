341dacecf21b226a14725afee9f120d71b7c3151
vagrant <vagrant@homestead>
2020-06-17 00:41:11 +0000

    Add bootstrap styling to input and button in tasks child view

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,9 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+
   <div class="panel panel-default">
     <div class="panel-body">
-        
+        <form action="/task" method="POST" class="form-horizontal">
+          <div class="form-group">
+            <label for="name" class="col-sm-3 control-label">Task</label>
+
+            <div class="col-sm-6">
+              <input type="text" name="name" id="name" class="form-control">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+              <button type="submit" class="btn btn-default">
+                Add Task
+              </button>  
+            </div>
+          </div>
+        </form>
     </div>
   </div>
+
 @endsection
